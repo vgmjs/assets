3adc959476712ce9099fc154c2c27d7acf98a30a
vgmjs <134774558+vgmjs@users.noreply.github.com>
2024-05-08 20:55:47 +0530

    Update extra.js

--- extra.js
+++ extra.js
@@ -1,1 +1,16 @@
+var gtagScript = document.createElement('script');
+gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-P36YDXY7SM';
+gtagScript.async = true;
+var customScript = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-P36YDXY7SM');
+`;
+gtagScript.onload = function() {
+  var customScriptElement = document.createElement('script');
+  customScriptElement.innerHTML = customScript;
+  document.head.appendChild(customScriptElement);
+};
 
+document.head.appendChild(gtagScript);
